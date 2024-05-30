c4e99aad80fe29bae94beb8190d50116b22d03f1
WassimSss <tojiikan@gmail.com>
2024-05-30 23:53:39 +0200

    add admin

--- controllers/exercises/muscle-groups.ts
+++ controllers/exercises/muscle-groups.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from 'express';
-import { IUser } from '../../models/users';
+import { IUser, User } from '../../models/users';
 import { checkData } from '../../modules/checkData';
 import { Exercise } from '../../models/exercises';
 import { MuscleGroup } from '../../models/muscleGroups';
+import { checkAdmin } from '../../modules/checkAdmin';
 
 declare module 'express' {
     
@@ -14,15 +15,29 @@ declare module 'express' {
 const add =  async (req : Request, res: Response) => {
   const idUser = req.user?.id;
 
+  const user = await User.findById(idUser) as IUser;
+  // Check if the user is an admin
+  if(checkAdmin(user, res)){
+    return;
+  }
   // Check if the user exists 
   if(checkData({ idUser: idUser }, res, 'Utilisateur non trouvé', false)){
     return;
   }
 
+  // Check if the name is provided
   const name = req.body.name;
-
    if(checkData({name: name}, res, "Le nom de l'exercice est requis", false)){
     return;
+  }
+
+  // Check if the muscle group already exists
+  const muscleGroupExists = await MuscleGroup.findOne({
+    name: name
+  });
+
+  if(checkData({muscleGroupExists: muscleGroupExists}, res, 'Ce groupe musculaire existe déjà', true)){
+    return ;
   }
 
   const newMuscleGroup = new MuscleGroup({
